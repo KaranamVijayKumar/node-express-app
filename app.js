e90b649ca9b8a8dc55d35804ad25e13d1b76c4bd
KaranamVijayKumar <karanamvijay960@gmail.com>
2019-03-08 16:19:34 -0600

    added app.use, home.html and css file

--- app.js
+++ app.js
@@ -1,23 +1,32 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
-const hostname = '127.0.0.1'   // set constants
+const hostname = '0.0.0.0'   // set constants
 const port = 3002
 
+app.use(express.static(path.join(__dirname, './styles')))
+
 app.get('/', function (req, res) {
-  res.send('Welcome home!')
+  res.sendFile(path.join(__dirname, '/home.html'))
+})
+
+app.use(function (req, res, next) {
+  console.log('logged')
+  next()
 })
 
 app.get('/hello', (req, res) => {
   res.send('Hello World!')
 })
 
+
 app.get('/big',  (req, res) =>{
   res.send('<h1>Hello World!</h1>')
 })
 
-app.get('/greeting/:id',  (req, res) =>{
-  res.send('Hello! The id was ' + req.params.id)
+app.get('/Vijay',  (req, res) =>{
+  res.send('<h1>Hello Vijay!</h1>')
 })
 
 app.get('/yo/:buddy',  (req, res) =>{
